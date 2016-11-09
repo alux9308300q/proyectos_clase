b1f38a857140d25b1b6ecea64c4e763097da75ef
george <george@george-VirtualBox>
2016-11-09 09:13:46 +0100

    Practica 4

---
+++ Practica4/src/practica4/Practica4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica4;
+
+/**
+ *
+ * @author george
+ */
+public class Practica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    /* Declarar operantes */
+    int num1;
+    int num2;
+    int resultado;
+    
+    /*Asignar operantes*/
+    num1=15;
+    num2=3;
+    
+    /* Operacion suma */
+    resultado=num1/num2;
+    
+    /*Mostrar resultado*/
+    System.out.println(resultado);
+    }
+    
+}
